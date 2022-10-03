441bf33c8e2dc73b273b85a483e2b70b95996df2
Lucas Soares Maia <lucasmaia@paracatu.grad.dcc.ufmg.br>
2022-10-03 16:41:55 -0300

    frist

--- stable.cpp
+++ stable.cpp
@@ -6,55 +6,36 @@
 
 // This function returns true if woman 'w' prefers man 'm1' over man 'm'
 
-bool wPrefersM1OverM(int prefer[2 * N][N], int w, int m, int m1)
-
-{
+bool wPrefersM1OverM(int prefer[2 * N][N], int w, int m, int m1) {
   // Check if w prefers m over her current engagment m1
 
-  for (int i = 0; i < N; i++)
-
-  {
+  for (int i = 0; i < N; i++) {
     // If m1 comes before m in the list of w,
-
     // then w prefers her current engagement, don't do anything
 
     if (prefer[w][i] == m1)
-
       return true;
 
     // If m cmes before m1 in w's list, then free her current
-
     // engagement and engage her with m
 
     if (prefer[w][i] == m)
-
       return false;
   }
 }
 
 // Prints stable matching for N boys and N girls. Boys are numbered as 0 to N-1.
-
 // Girls are numbered as N to 2N-1.
 
-void stableMarriage(int prefer[2 * N][N])
-
-{
-  /*
-
-   Stores partner of women. This is our output array that stores information.
-
+void stableMarriage(int prefer[2 * N][N]) {
+  /* Stores partner of women. This is our output array that stores information.
    The value of wPartner[I] indicates the partner assigned to woman N+i.
-
    Note that the woman numbers between N and 2*N-1.
-
-   The value -1 indicates that (N+i)'th woman is free
-
-  */
+   The value -1 indicates that (N+i)'th woman is free */
 
   int wPartner[N];
 
   // An array to store availability of men. If mFree[i] is false,
-
   // then man 'i' is free, otherwise engaged.
 
   bool mFree[N];
@@ -70,26 +51,19 @@ void stableMarriage(int prefer[2 * N][N])
 
   // While there are free men
 
-  while (freeCount > 0)
-
-  {
+  while (freeCount > 0) {
     // Pick the first free man (we could pick any)
 
     int m;
 
     for (m = 0; m < N; m++)
-
       if (mFree[m] == false)
-
         break;
 
     // One by one go to all women according to m's preferences.
-
     // Here m is the picked free man
 
-    for (int i = 0; i < N && mFree[m] == false; i++)
-
-    {
+    for (int i = 0; i < N && mFree[m] == false; i++) {
       int w = prefer[m][i];
 
       // The woman of preference is free, w and m become partners.
